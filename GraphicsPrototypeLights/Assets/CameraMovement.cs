c8b7989c988830152c2362a95c00a668fb66042f
Sravan Kumar <knight2356@gmail.com>
2017-03-09 09:57:52 -0500

    Basic Camera Movement Done.

---
+++ GraphicsPrototypeLights/Assets/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform PlayerTransform;
+
+    public float YOffset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 CamPos = GetComponent<Transform>().position;
+        GetComponent<Transform>().position = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y + YOffset, CamPos.z);
+	}
+}
